d8f5aa9bd048a2bbebc28d13169d4dc16b06f533
Sulaiman AlQanaei <salqanaei@Sulaimans-MacBook-Air-2.local>
2019-07-08 20:39:26 +0300

    Draft

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
+def home(request):
+	context = {
+		"msg" : "Hello World!"
+
+	}
+	return render(request, 'restaurants.html', context)
+
 # Create your views here.
